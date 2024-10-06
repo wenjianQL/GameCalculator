444de282d834132165bd31cf11b1047fa1e0c887
wenjianQL <674179535@qq.com>
2024-10-06 10:01:40 +0800

    [fix]：修改配方后，物品顺序会排到最后的问题。

--- js/test/treeListShow.js
+++ js/test/treeListShow.js
@@ -6,16 +6,22 @@
  */
 function renderTree(ulNode, data) {
     if (!data) return null;
-    console.log("data = ");
-    console.log(data);
-
-    // ulNode为空，返回
-    // if (!ulNode) return null;
 
     // 根据物品名字和数量，在ulNode中添加对应的li进行结果显示
     const currentNode = document.createElement('li');
 
-    // 如果childNodeList不为空，那么计算需要的设备数量
+    // 设置li节点内容
+    setLiContent(currentNode, data);
+
+    ulNode.appendChild(currentNode);
+
+    return ulNode;
+}
+
+/**
+ * 给li节点添加：物品名称和数量、设备数量、配方选择select
+ */
+function setLiContent(liNode, data) {
     if (data.childNodeList.length !== 0) {
         let needDeviceNumber = 0;
         // 根据设备类型，获取设备
@@ -24,30 +30,11 @@ function renderTree(ulNode, data) {
         needDeviceNumber = Math.ceil((data.number / (data.oneEquProductNumber * equ.rate)) * 100) / 100;
         const textNode = document.createTextNode(
             `${data.number}*${data.name}（${needDeviceNumber}*${equ.name}）`);
-        currentNode.appendChild(textNode);
+        liNode.appendChild(textNode);
     } else {
         const textNode = document.createTextNode(`${data.number}*${data.name}`);
-        currentNode.appendChild(textNode);
+        liNode.appendChild(textNode);
     }
-
-    // TODO 忽略按钮
-    // const button = document.createElement('button');
-    // // button设置点击事件
-    // button.addEventListener('click', () => {
-    //     // TODO：
-    //     // 获取data中的isCalculator，并取反重新设置，然后重新渲染当前节点
-    //     // data.isCalculator = !data.isCalculator;
-    //     // renderTree(ulNode, data);
-    // });
-    // // 根据isCalculator设置按钮文本和样式
-    // if (data.isCalculator) {
-    //     button.textContent = '忽略';
-    // } else {
-    //     button.textContent = '计算';
-    //     currentNode.style.textDecoration = 'line-through';
-    // }
-    // currentNode.appendChild(button);
-
 
     // 如果data中的childNodeList不为空
     if (data.childNodeList.length !== 0) {
@@ -69,29 +56,23 @@ function renderTree(ulNode, data) {
             const selectIdArr = selectId.split("_");
             const path = selectIdArr[0];
             const index = selectIdArr[1];
-            console.log(path, index);
 
             treeNodeRecipeIndexMap[path] = index;
 
             // 获取当前节点的新计算结果
             const newData = getCalculateResult(path.substring(0, path.lastIndexOf('-')), data.name, data.number);
-            console.log("newData = ");
-            console.log(newData);
             // 数据更新
             data.otherProductList = newData.otherProductList;
             data.childNodeList = newData.childNodeList;
             data.nodeRecipeIndex = newData.nodeRecipeIndex;
             data.equType = newData.equType;
             data.oneEquProductNumber = newData.oneEquProductNumber;
-            // 删除currentNode
-            ulNode.removeChild(currentNode);
-            // 重新渲染节点ulNode
-            renderTree(ulNode, data);
-        });
-        currentNode.appendChild(select);
-    }
 
-    ulNode.appendChild(currentNode);
+            liNode.innerHTML = "";
+            setLiContent(liNode, data);
+        });
+        liNode.appendChild(select);
+    }
 
     // 如果otherProductList不为空或sourceList不为空
     if (data.otherProductList || data.childNodeList) {
@@ -99,7 +80,7 @@ function renderTree(ulNode, data) {
         const childUlNode = document.createElement('ul');
         childUlNode.id = data.path;
         childUlNode.style.display = data.isExpand ? 'block' : 'none';
-        currentNode.appendChild(childUlNode);
+        liNode.appendChild(childUlNode);
 
         const toggleButton = document.createElement('span');
         toggleButton.textContent = data.isExpand ? '▼' : '►';
@@ -110,7 +91,7 @@ function renderTree(ulNode, data) {
             childUlNode.style.display = data.isExpand ? 'block' : 'none';
             toggleButton.textContent = data.isExpand ? '▼' : '►';
         });
-        currentNode.insertBefore(toggleButton, currentNode.firstChild);
+        liNode.insertBefore(toggleButton, liNode.firstChild);
 
         // 如果sourceList不为空
         if (data.childNodeList) {
@@ -131,6 +112,4 @@ function renderTree(ulNode, data) {
             }
         }
     }
-
-    return ulNode;
 }
